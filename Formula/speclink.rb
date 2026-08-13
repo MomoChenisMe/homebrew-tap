83a3cb37161b3a22f702d0cb6b4e18f505211273
MomoChen <43069170+MomoChenisMe@users.noreply.github.com>
2026-08-13 06:45:06 +0000

    speclink v0.1.2

--- Formula/speclink.rb
+++ Formula/speclink.rb
@@ -2,26 +2,26 @@
 class Speclink < Formula
   desc "Spec-Driven Development engine and toolkit"
   homepage "https://github.com/MomoChenisMe/speclink"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.1/speclink-v0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "c12c64b85d411b742b42876accb0cc433caf7dc199f73a417994d508f8923f0d"
+      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.2/speclink-v0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "35ca4f1d16f8649672d6f92bfc6fefe79826e4471647d6de880d70ab8f2b10b2"
     else
-      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.1/speclink-v0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "05f9bfd47394894632816d4b2ed7c749f1726cd6551e4e3ac32be3722d377854"
+      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.2/speclink-v0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "094520b3a79ba6f8bdb93bee9fb7bbe7d4658aeb7ed818266d0fa0cc6f1b126d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.1/speclink-v0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "df20ca598ddf5efe862c2625f4b00a89f4f48ec43548d31e1305c3d3ed0063f7"
+      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.2/speclink-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ff176c9ff89e85414ce16394153d70dd24b22007b0dac463bf07eeb60f52469a"
     else
-      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.1/speclink-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "37e06cb13411f98f08483061f07f7ca21d746758e1cb2365cdd99c730a4c32ad"
+      url "https://github.com/MomoChenisMe/speclink/releases/download/v0.1.2/speclink-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "934d5419fad529d4633427adb0076f8c2f700cd013f06bdf456bc14c12181dc1"
     end
   end
 
@@ -30,6 +30,6 @@ class Speclink < Formula
   end
 
   test do
-    assert_match "0.1.1", shell_output("#{bin}/speclink --version")
+    assert_match "0.1.2", shell_output("#{bin}/speclink --version")
   end
 end
