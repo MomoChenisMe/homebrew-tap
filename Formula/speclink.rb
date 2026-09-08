aff0fe396151f139e3dc3c947c07e7f08d06189d
MomoChen <43069170+MomoChenisMe@users.noreply.github.com>
2026-09-08 02:58:14 +0000

    speclink v0.2.0

--- Formula/speclink.rb
+++ Formula/speclink.rb
@@ -8,20 +8,20 @@ class Speclink < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/MomoChenisMe/speclink/releases/download/v0.2.0/speclink-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ff9d87885c18600dfe4e2f613a27fee32e4b855dfcb7655e6df5a69910463821"
+      sha256 "b357a69f34654683779f84c111bbb73fed5eec537bc4ebc09a01dc10c4b0adea"
     else
       url "https://github.com/MomoChenisMe/speclink/releases/download/v0.2.0/speclink-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "514b52d8319e1c6493540fdb15fb219d3ac78a8acc6a280d69f0e9500cf5e36b"
+      sha256 "ef85577b847ead25ade37eb754bac27269ad8bfe31665223b0046a20630392f8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/MomoChenisMe/speclink/releases/download/v0.2.0/speclink-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "18704bcfbe4931fccb6bad2ffd556bc8c524819b280e8f1cce83509d0efe7c14"
+      sha256 "d5a46674d1fe9d45dbc565930b533230f594da7dd4790f6ecdf156318586e539"
     else
       url "https://github.com/MomoChenisMe/speclink/releases/download/v0.2.0/speclink-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "78ae94a24063c591e8d468be7ad834598bbb261071df866565b38da6a896f91c"
+      sha256 "b7f16f0f04a8c0673d5c875d216e3a704558b352ba2350a06de661d4a897bff1"
     end
   end
 
