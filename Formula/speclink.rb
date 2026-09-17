900b6a78e4cb3152180bf41fe4ab285c8901f55c
MomoChen <43069170+MomoChenisMe@users.noreply.github.com>
2026-09-17 09:16:08 +0000

    speclink v0.6.0

--- Formula/speclink.rb
+++ Formula/speclink.rb
@@ -2,26 +2,26 @@
 class Speclink < Formula
   desc "Spec-Driven Development engine and toolkit"
   homepage "https://github.com/MomoChenisMe/speclink"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://registry.npmjs.org/@speclink/cli-darwin-arm64/-/cli-darwin-arm64-0.5.0.tgz"
-      sha256 "6a9b8da17d72c7446a663b5e7bf5a24401789e6ccb38722995bbd7a87dff2036"
+      url "https://registry.npmjs.org/@speclink/cli-darwin-arm64/-/cli-darwin-arm64-0.6.0.tgz"
+      sha256 "5f3e1d6b8223bf5db7321d49c10a2c218a823d5e427cfff57bf9255d972a6c77"
     else
-      url "https://registry.npmjs.org/@speclink/cli-darwin-x64/-/cli-darwin-x64-0.5.0.tgz"
-      sha256 "a8acb2cb9ae87874ad058ac2ef4c45f95b22f0308d3d0ad3a09e9d18ec84f264"
+      url "https://registry.npmjs.org/@speclink/cli-darwin-x64/-/cli-darwin-x64-0.6.0.tgz"
+      sha256 "9c2a5cd8689c8904c158a74359b8f108ce3c5cab1b32f3703b4566614e380495"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://registry.npmjs.org/@speclink/cli-linux-arm64/-/cli-linux-arm64-0.5.0.tgz"
-      sha256 "37db2eaad2a5e339853a0adb7cb948d06da1f9a94888c57fb9f68b6d9438f495"
+      url "https://registry.npmjs.org/@speclink/cli-linux-arm64/-/cli-linux-arm64-0.6.0.tgz"
+      sha256 "c7eefb36b21d014210312f7aa4596c5e079d789cfac89e28bd6f18e1af609c83"
     else
-      url "https://registry.npmjs.org/@speclink/cli-linux-x64/-/cli-linux-x64-0.5.0.tgz"
-      sha256 "249359bbd9c7f7bd05e82274bf69c5b6cfc68b5eec93b4a5988c6b370df29835"
+      url "https://registry.npmjs.org/@speclink/cli-linux-x64/-/cli-linux-x64-0.6.0.tgz"
+      sha256 "b766afed29c47d61ad8c9272758fdddcc8c76cd46f344077b87a68dd48bb4b71"
     end
   end
 
@@ -30,6 +30,6 @@ class Speclink < Formula
   end
 
   test do
-    assert_match "0.5.0", shell_output("#{bin}/speclink --version")
+    assert_match "0.6.0", shell_output("#{bin}/speclink --version")
   end
 end
